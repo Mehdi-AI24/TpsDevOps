813bb6ecfecc50419241b93586eb1f187f9d25d7
Mehdi-AI24 <mehdiaititto239@gmail.com>
2024-05-13 09:52:57 +0100

    Tps DevOPs

---
+++ src/main/java/com/example/tpsdevops/TpsDevOpsApplication.java
@@ -0,0 +1,14 @@
+package com.example.tpsdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpsDevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpsDevOpsApplication.class, args);
+        System.out.println("Tps DevOps: Jenkins");
+    }
+
+}
---
+++ src/test/java/com/example/tpsdevops/TpsDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpsdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpsDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
